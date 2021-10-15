ead78674eb1fa2d06e02bf7212b8f556c715bb4a
Alexander Tolikin <atolikin@nvidia.com>
2021-10-15 13:21:13 +0000

    Added configs, WSGI, checked positive work flow

--- plugins/UFM_NDT_Plugin/ufm_sim_web_service/ufm_web_sim.py
+++ plugins/UFM_NDT_Plugin/ufm_sim_web_service/ufm_web_sim.py
@@ -14,8 +14,15 @@
 @date:   September 20, 2021
 """
 
+import configparser
 from flask import Flask
 from flask_restful import Api
+import logging
+import os
+
+from twisted.web.wsgi import WSGIResource
+from twisted.internet import reactor
+from twisted.web import server
 
 from resources import Compare, Ndts, Reports, ReportId, UploadMetadata, Delete
 
@@ -23,24 +30,38 @@ from resources import Compare, Ndts, Reports, ReportId, UploadMetadata, Delete
 class UFMWebSim:
 
     def __init__(self):
-        # Create routes
         self.port_number = 8980
         self.app = Flask(__name__)
         api = Api(self.app)
+        log_level = logging.INFO
+        ndt_config = configparser.ConfigParser()
+        # config_file_name = "/config/ndt.conf"
+        config_file_name = "../build/config/ndt.conf"
+        if os.path.exists(config_file_name):
+            ndt_config.read(config_file_name)
+            log_level = ndt_config.get("Common", "debug_level",
+                                       fallback=logging.INFO)
+        logging.basicConfig(filename="/tmp/ndt.log",
+                            level=logging._nameToLevel[log_level])
+        # logging.basicConfig(filename="/data/ndt.log",
+        #                     level=logging._nameToLevel[log_level])
 
         apis = {
-            Compare: "/ufmRestV2/plugin/ndt/compare",
-            Ndts: "/ufmRestV2/plugin/ndt/list",
-            Reports: "/ufmRestV2/plugin/ndt/reports",
-            Delete: "/ufmRestV2/plugin/ndt/delete",
-            UploadMetadata: "/ufmRestV2/plugin/ndt/upload_metadata",
-            ReportId: "/ufmRestV2/plugin/ndt/reports/<report_id>",
+            Compare: "/plugin/ndt/compare",
+            Ndts: "/plugin/ndt/list",
+            Reports: "/plugin/ndt/reports",
+            Delete: "/plugin/ndt/delete",
+            UploadMetadata: "/plugin/ndt/upload_metadata",
+            ReportId: "/plugin/ndt/reports/<report_id>",
         }
         for resource, path in apis.items():
             api.add_resource(resource, path)
 
-    def run(self):
-        self.app.run(host='0.0.0.0', port=self.port_number, debug=True)
+    async def run(self):
+        self.app.run(port=self.port_number, debug=True)
+        # resource = WSGIResource(reactor, reactor.getThreadPool(), self.app)
+        # reactor.listenTCP(self.port_number, server.Site(resource))
+        # reactor.run()
 
     async def stop(self):
         pass
