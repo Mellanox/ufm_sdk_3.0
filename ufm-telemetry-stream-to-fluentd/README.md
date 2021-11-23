c14f33ad4f2e18c024953299579d13a4951d1c1b
ananalaghbar <anana@nvidia.com>
2021-11-23 16:10:45 +0200

    2785166 : Streaming UFM Telemetry to fluentd

---
+++ ufm-telemetry-stream-to-fluentd/README.md
@@ -0,0 +1,70 @@
+UFM Telemetry endpoint stream To FluentD endpoint
+--------------------------------------------------------
+
+
+This plugin is used to extract ufm telemetry via [Prometheus](https://prometheus.io/) metrics and stream it via [fluentd](https://www.fluentd.org/) protocol to telemetry console
+
+Overview
+--------------------------------------------------------
+
+NVIDIA UFM Telemetry platform provides network validation tools to monitor network performance and conditions, capturing and streaming rich real-time network telemetry information, application workload usage to an on-premise or cloud-based database for further analysis.
+As a fabric manager, the UFM Telemetry holds a real-time network telemetry information of the network topology. This information should be reflected, over time (as it can change with time) towards telemetry console. In order to do so, we present stream To FluentD plugin
+
+
+
+Prerequisites
+--------------------------------------------------------
+
+To install all prerequisites, please run :
+
+    pip install -r requirements.txt
+
+Fluentd configurations
+--------------------------------------------------------
+
+- We provide [fluentd.conf](fluentd.conf) as a fluentd configurations sample.
+
+Run
+--------------------------------------------------------
+### 1) using command line arguments :
+
+
+    python3 ufm-telemetry-stream-to-fluentd.py --ufm_telemetry_host=<host> --ufm_telemetry_port=<port> --fluentd_host=fluentd_host --fluentd_port=fluentd_port  
+
+
+### 2) using configuration file:
+
+  - copy config file sample ufm-telemetry-stream-to-fluentd.sample.cfg to ufm-telemetry-stream-to-fluentd.cfg
+
+
+    cp ufm-telemetry-stream-to-fluentd.sample.cfg ufm-telemetry-stream-to-fluentd.cfg
+
+  - Edit config file with relevant parameters
+
+
+    vi ufm-telemetry-stream-to-fluentd.cfg
+
+  - Run
+
+
+    python3 ufm-telemetry-stream-to-fluentd.py
+
+ Running syntax
+--------------------------------------------------------
+
+| Argument | Corresponding Config Value | Required | Description |
+| :---: | :---: |:---: |:---: |
+| --fluentd_host <fluentd_host> | [fluentd-endpoint.host](ufm-telemetry-stream-to-fluentd.sample.cfg#L6) | True |  Hostname or IP for FluentD endpoint
+| --fluentd_port <fluentd_port> | [fluentd-endpoint.port](ufm-telemetry-stream-to-fluentd.sample.cfg#L7) | True | Port for FluentD endpoint
+| --fluentd_timeout <fluentd_timeout> | [fluentd-endpoint.timeout](ufm-telemetry-stream-to-fluentd.sample.cfg#L8) | True | Timeout for FluentD endpoint streaming [Default is 120 seconds]
+| --fluentd_message_tag_name <fluentd_message_tag_name> | [fluentd-endpoint.message_tag_name](ufm-telemetry-stream-to-fluentd.sample.cfg#L10) | False | Message Tag Name for FluentD endpoint message [Default is the ufm_telemetry_host]
+| --ufm_telemetry_host <ufm_telemetry_host> | [ufm-telemetry-endpoint.host](ufm-telemetry-stream-to-fluentd.sample.cfg#L2) | True | Hostname or IP for The UFM Telemetry Endpoint
+| --ufm_telemetry_port <ufm_telemetry_port> | [ufm-telemetry-endpoint.port](ufm-telemetry-stream-to-fluentd.sample.cfg#L3) | True | Port for The UFM Telemetry Endpoint [Default is 9001]
+| --logs_file_name <logs_file_name> | [logs-config.logs_file_name](../conf/ufm-sdk.sample.cfg#L11) | False | Log file name [Default = 'console.log']
+| --logs_level <logs_level> | [logs-config.logs_level](../conf/ufm-sdk.sample.cfg#L14) | False | Default is 'info'
+
+*If command line argument is provided, the corresponding config value will be ignored
+
+Use
+--------------------------------------------------------
+This application is not a daemon; you should run it via time-based job scheduler (cron job).
---
+++ ufm-telemetry-stream-to-fluentd/ufm-telemetry-stream-to-fluentd.py
@@ -0,0 +1,176 @@
+#!/usr/bin/python3
+
+"""
+@copyright:
+    Copyright (C) Mellanox Technologies Ltd. 2014-2020.  ALL RIGHTS RESERVED.
+
+    This software product is a proprietary product of Mellanox Technologies
+    Ltd. (the "Company") and all right, title, and interest in and to the
+    software product, including all associated intellectual property rights,
+    are and shall remain exclusively with the Company.
+
+    This software product is governed by the End User License Agreement
+    provided with the software product.
+
+@author: Anan Al-Aghbar
+@date:   Nov 23, 2021
+"""
+import requests
+import logging
+import time
+import datetime
+from fluent import asyncsender as asycsender
+
+try:
+    from utils.utils import Utils
+    from utils.ufm_rest_client import UfmRestClient, HTTPMethods
+    from utils.args_parser import ArgsParser
+    from utils.config_parser import ConfigParser
+    from utils.logger import Logger, LOG_LEVELS
+except ModuleNotFoundError as e:
+    print("Error occurred while importing python modules, "
+          "Please make sure that you exported your repository to PYTHONPATH by running: "
+          f'export PYTHONPATH="${{PYTHONPATH}}:{os.path.dirname(os.getcwd())}"')
+
+
+class UFMTelemetryConstants:
+    PLUGIN_NAME = "UFM_Telemetry_Streaming"
+    TELEMETRY_PROMETHEUS_METRICS = "labels/metrics"
+
+    args_list = [
+        {
+            "name": '--ufm_telemetry_host',
+            "help": "Host or IP of UFM Telemetry endpoint"
+        },{
+            "name": '--ufm_telemetry_port',
+            "help": "Port of UFM Telemetry endpoint"
+        },{
+            "name": '--fluentd_host',
+            "help": "Host name or IP of fluentd endpoint"
+        },{
+            "name": '--fluentd_port',
+            "help": "Port of fluentd endpoint"
+        },{
+            "name": '--fluentd_timeout',
+            "help": "Fluentd timeout in seconds"
+        },{
+            "name": '--fluentd_message_tag_name',
+            "help": "Tag name of fluentd endpoint message"
+        }
+    ]
+
+
+class UFMTelemetryStreamingConfigParser(ConfigParser):
+    config_file = "ufm-telemetry-stream-to-fluentd.cfg"
+
+    UFM_TELEMETRY_ENDPOINT_SECTION = "ufm-telemetry-endpoint"
+    UFM_TELEMETRY_ENDPOINT_SECTION_HOST = "host"
+    UFM_TELEMETRY_ENDPOINT_SECTION_PORT = "port"
+
+    FLUENTD_ENDPOINT_SECTION = "fluentd-endpoint"
+    FLUENTD_ENDPOINT_SECTION_HOST = "host"
+    FLUENTD_ENDPOINT_SECTION_PORT = "port"
+    FLUENTD_ENDPOINT_SECTION_TIMEOUT = "timeout"
+    FLUENTD_ENDPOINT_SECTION_MSG_TAG_NAME = "message_tag_name"
+
+    def __init__(self, args):
+        super().__init__(args)
+        self.sdk_config.read(self.config_file)
+
+    def get_telemetry_host(self):
+        return self.get_config_value(self.args.ufm_telemetry_host,
+                                     self.UFM_TELEMETRY_ENDPOINT_SECTION,
+                                     self.UFM_TELEMETRY_ENDPOINT_SECTION_HOST)
+
+    def get_telemetry_port(self):
+        return self.safe_get_int(self.args.ufm_telemetry_port,
+                                 self.UFM_TELEMETRY_ENDPOINT_SECTION,
+                                 self.UFM_TELEMETRY_ENDPOINT_SECTION_PORT,
+                                 9001)
+
+    def get_fluentd_host(self):
+        return self.get_config_value(self.args.fluentd_host,
+                                     self.FLUENTD_ENDPOINT_SECTION,
+                                     self.FLUENTD_ENDPOINT_SECTION_HOST)
+
+    def get_fluentd_port(self):
+        return self.safe_get_int(self.args.fluentd_port,
+                                 self.FLUENTD_ENDPOINT_SECTION,
+                                 self.FLUENTD_ENDPOINT_SECTION_PORT)
+
+    def get_fluentd_timeout(self):
+        return self.safe_get_int(self.args.fluentd_port,
+                                 self.FLUENTD_ENDPOINT_SECTION,
+                                 self.FLUENTD_ENDPOINT_SECTION_TIMEOUT,
+                                 120)
+
+    def get_fluentd_msg_tag(self,default=None):
+        return self.get_config_value(self.args.fluentd_host,
+                                     self.FLUENTD_ENDPOINT_SECTION,
+                                     self.FLUENTD_ENDPOINT_SECTION_MSG_TAG_NAME,
+                                     default)
+
+
+class UFMTelemetryStreaming:
+
+    @staticmethod
+    def get_metrics(host, port):
+        url = f'http://{host}:{port}/{UFMTelemetryConstants.TELEMETRY_PROMETHEUS_METRICS}'
+        logging.info(f'Send UFM Telemetry Endpoint Request, Method: GET, URL: {url}')
+        try:
+            response = requests.get(url)
+            return response.text
+        except Exception as e:
+            logging.error(e)
+            return response
+
+    @staticmethod
+    def stream_data_to_fluentd(fluentd_host,
+                               fluentd_port,
+                               fluentd_msg_name,
+                               fluentd_timeout,data_to_stream):
+        logging.info(f'Streaming to Fluentd IP: {fluentd_host} port: {fluentd_port} timeout: {fluentd_timeout}')
+        try:
+            fluent_sender = asycsender.FluentSender(UFMTelemetryConstants.PLUGIN_NAME,
+                                                    fluentd_host,
+                                                    fluentd_port, timeout=fluentd_timeout)
+
+            fluentd_message = {
+                "timestamp": datetime.datetime.fromtimestamp(int(time.time())).strftime('%Y-%m-%d %H:%M:%S'),
+                "type": "full",
+                "metrics": data_to_stream
+            }
+
+            fluent_sender.emit(fluentd_msg_name, fluentd_message)
+            fluent_sender.close()
+            logging.info(f'Finished Streaming to Fluentd Host: {fluentd_host} port: {fluentd_port}')
+        except Exception as e:
+            logging.error(e)
+
+if __name__ == "__main__":
+    # init app args
+    args = ArgsParser.parse_args("UFM Telemetry Streaming to fluentd", UFMTelemetryConstants.args_list)
+
+    # init app config parser & load config files
+    config_parser = UFMTelemetryStreamingConfigParser(args)
+
+    # init logs configs
+    logs_file_name = config_parser.get_logs_file_name()
+    logs_level = config_parser.get_logs_level()
+    Logger.init_logs_config(logs_file_name, logs_level)
+
+    ufm_telemetry_host = config_parser.get_telemetry_host()
+    ufm_telemetry_port = config_parser.get_telemetry_port()
+
+    fluentd_host = config_parser.get_fluentd_host()
+    fluentd_port = config_parser.get_fluentd_port()
+    fluentd_timeout = config_parser.get_fluentd_timeout()
+    fluentd_msg_tag = config_parser.get_fluentd_msg_tag(ufm_telemetry_host)
+
+
+    metrics = UFMTelemetryStreaming.get_metrics(ufm_telemetry_host,
+                                                ufm_telemetry_port)
+
+    UFMTelemetryStreaming.stream_data_to_fluentd(fluentd_host, fluentd_port, fluentd_msg_tag,
+                                                 fluentd_timeout, metrics)
+
