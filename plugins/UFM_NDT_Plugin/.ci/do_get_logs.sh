d203f771e76791dba712f4bb76045bff9efc1bd4
Kaidrikov Evgeny <ekaidrikov@nvidia.com>
2022-03-23 12:28:23 +0200

    adding CI

--- plugins/UFM_NDT_Plugin/.ci/do_get_logs.sh
+++ plugins/UFM_NDT_Plugin/.ci/do_get_logs.sh
@@ -13,15 +13,15 @@ send -- "config terminal\r"
 expect "/(config/) # "
 send -- "debug generate dump\r"
 expect "/(config/) # "
-send -- "file debug-dump upload latest scp://root:3tango@${NODE_NAME}:/tmp/ndt\r"
+send -- "file debug-dump upload latest scp://root:3tango@${NODE_NAME}:/$WORKSPACE\r"
 expect "/(config/) # "
 send -- "file debug-dump delete latest\r"
 expect "/(config/) # "
 send -- "exit\r"
 sleep 10
 EOF
-cd /tmp/ndt
-tar -zxvf $(ls /tmp/ndt/sysdump-ufm-appliance*.tgz)
+cd /$WORKSPACE
+tar -zxvf $(ls /$WORKSPACE/sysdump-ufm-appliance*.tgz)
 rm -rf sysdump-ufm-appliance*.tgz
 cd sysdump-ufm-appliance*
 tar -zxvf ufm-sysdump*
