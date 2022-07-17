559b4c88e2da0a3f3408181b67b14b02b50f07b0
nasraj <nasraj@nvidia.com>
2022-07-17 14:03:51 +0300

    load ports script

---
+++ scripts/ufm_ports/README.md
@@ -0,0 +1,43 @@
+UFM Devices Action
+--------------------------------------------------------
+
+
+This plugin is used to get ports via the UFM APIs.
+
+
+Prerequisites
+--------------------------------------------------------
+
+To install all prerequisites, please run :
+
+    pip install -r requirements.txt
+
+To exported your repository to PYTHONPATH, please run :
+
+   export PYTHONPATH="${PYTHONPATH}:<your ufm_sdk_cookbook path>"
+
+
+Running using command line arguments :
+--------------------------------------------------------
+
+    python3 load_ports.py
+
+ Running syntax
+--------------------------------------------------------
+
+| Argument | Corresponding Config Value | Required | Description |
+| :---: | :---: |:---: |:---: |
+| --ufm_host <ufm_host> | [ufm-remote-server-config.host](../conf/ufm-devices.sample.cfg#L2) | True | Hostname or IP for The UFM Enterprise
+| --ufm_protocol <ufm_protocol> | [ufm-server-config.ws_protocol](../conf/ufm-devices.sample.cfg#L4) | True | Web services protocol used by UFM Enterprise (HTTP, HTTPS)
+| --ufm_access_token <ufm_access_token> | [ufm-server-config.access_token](../conf/ufm-devices.sample.cfg#L5) | True | Access Token of UFM
+| --ufm_username <ufm_username> | [ufm-server-config.username](../conf/ufm-devices.sample.cfg#L6) | True | Username of UFM user
+| --ufm_password <ufm_password> | [ufm-server-config.password](../conf/ufm-devices.sample.cfg#L7) | True | Password of UFM user
+| --logs_file_name <logs_file_name> | [logs-config.logs_file_name](../conf/ufm-devices.sample.cfg#L11) | False | Log file name [Default = 'console.log']
+| --logs_level <logs_level> | [logs-config.logs_level](../conf/ufm-devices.sample.cfg#L14) | False | Default is 'info'
+| --logs_level <logs_level> | [logs-config.logs_level](../conf/ufm-devices.sample.cfg#L14) | False | Default is 'info'
+| --system <system> | None | False | Option to get specific system ports data
+| --active <active> | None | False | Option to get active ports data only [Default = True]
+| --show_disabled <show_disabled> | None | False | Option to get show disabled ports data [Default = False]
+
+*If command line argument is provided, the corresponding config value will be ignored
+
---
+++ scripts/ufm_ports/load_ports.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python3
+
+"""
+@copyright:
+    Copyright (C) Nvidia Technologies Ltd. 2014-2022.  ALL RIGHTS RESERVED.
+
+    This software product is a proprietary product of Nvidia Technologies
+    Ltd. (the "Company") and all right, title, and interest in and to the
+    software product, including all associated intellectual property rights,
+    are and shall remain exclusively with the Company.
+
+    This software product is governed by the End User License Agreement
+    provided with the software product.
+
+@author: Nasr Ajaj
+@date:   Jul 14, 2022
+"""
+
+import os
+import platform
+from http import HTTPStatus
+
+try:
+    from utils.utils import Utils
+    from utils.ufm_rest_client import UfmRestClient, HTTPMethods
+    from utils.args_parser import ArgsParser
+    from utils.config_parser import ConfigParser
+    from utils.logger import Logger, LOG_LEVELS
+    from utils.exception_handler import ExceptionHandler
+except ModuleNotFoundError as e:
+    if platform.system() == "Windows":
+        print("Error occurred while importing python modules, "
+              "Please make sure that you exported your repository to PYTHONPATH by running: "
+              f'set PYTHONPATH=%PYTHONPATH%;{os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))}')
+    else:
+        print("Error occurred while importing python modules, "
+              "Please make sure that you exported your repository to PYTHONPATH by running: "
+              f'export PYTHONPATH="${{PYTHONPATH}}:{os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))}"')
+
+
+class UfmPortsConstants:
+
+    PORTS_API_URL = 'resources/ports'
+    API_PARAM_SHOW_DISABLED = "show_disabled"
+    API_PARAM_SYSTEM = "system"
+    API_PARAM_ACTIVE = "active"
+
+    args_list = [
+        {
+            "name": f'--{API_PARAM_SYSTEM}',
+            "help": "Option to get all ports data related to specific system node"
+        },
+        {
+            "name": f'--{API_PARAM_ACTIVE}',
+            "default": True,
+            "help": "Option to get active ports only"
+        },
+        {
+            "name": f'--{API_PARAM_SHOW_DISABLED}',
+            "default": False,
+            "help": "Option to get disabled ports"
+        }
+    ]
+
+
+class UfmPortsConfigParser(ConfigParser):
+    def __init__(self, args):
+        super().__init__(args)
+
+
+class UfmPortsManagement:
+
+    @staticmethod
+    def get_ports(active, show_disabled, system=None):
+        url = f'{UfmPortsConstants.PORTS_API_URL}?{UfmPortsConstants.API_PARAM_ACTIVE}={active}&{UfmPortsConstants.API_PARAM_SHOW_DISABLED}={show_disabled}'
+        if system:
+            url = f'{url}&{UfmPortsConstants.API_PARAM_SYSTEM}={system}'
+        response = ufm_rest_client.send_request(url)
+        if response and response.status_code == HTTPStatus.OK:
+            Logger.log_message(response.json())
+        else:
+            Logger.log_message(response, LOG_LEVELS.ERROR)
+        return response
+
+
+if __name__ == "__main__":
+    # init app args
+    args = ArgsParser.parse_args("UFM Ports Management", UfmPortsConstants.args_list)
+
+    # init app config parser & load config files
+    config_parser = UfmPortsConfigParser(args)
+
+    # init logs configs
+    logs_level = config_parser.get_logs_level()
+    logs_file_name = config_parser.get_logs_file_name()
+    Logger.init_logs_config(logs_file_name, logs_level)
+
+    # init ufm rest client
+    ufm_rest_client = UfmRestClient(host=config_parser.get_ufm_host(),
+                                    client_token=config_parser.get_ufm_access_token(), username=config_parser.get_ufm_username(),
+                                    password=config_parser.get_ufm_password(), ws_protocol=config_parser.get_ufm_protocol())
+    args_dict = args.__dict__
+    param_active = config_parser.safe_get_bool(args_dict.get(UfmPortsConstants.API_PARAM_ACTIVE), None, None, True)
+    param_show_disabled = config_parser.safe_get_bool(args_dict.get(UfmPortsConstants.API_PARAM_SHOW_DISABLED), None, None, False)
+    if args_dict.get(UfmPortsConstants.API_PARAM_SYSTEM):
+        system = args_dict.get(UfmPortsConstants.API_PARAM_SYSTEM)
+        UfmPortsManagement.get_ports(param_active, param_show_disabled, system)
+    else:
+        UfmPortsManagement.get_ports(param_active, param_show_disabled)
+
