84b4abf363ee4c4b35913e7ca806ac26d0ae10ea
Amit Gueta <agueta@nvidia.com>
2023-01-18 15:09:20 +0200

    add initial ci

--- .ci/cidemo-init.sh
+++ .ci/cidemo-init.sh
@@ -10,8 +10,8 @@ env
 changed_files=$(git diff --name-only remotes/origin/$ghprbTargetBranch)
 echo $changed_files
 echo "------------------------------"
-UFM_NDT_Plugin=$(git diff --name-only remotes/origin/$ghprbTargetBranch |grep UFM_NDT_Plugin |grep -v .gitmodules |wc -l)
-total=$(git diff --name-only remotes/origin/$ghprbTargetBranch |grep -v .gitmodules |wc -l)
+# UFM_NDT_Plugin=$(git diff --name-only remotes/origin/$ghprbTargetBranch |grep UFM_NDT_Plugin |grep -v .gitmodules |wc -l)
+# total=$(git diff --name-only remotes/origin/$ghprbTargetBranch |grep -v .gitmodules |wc -l)
 # if [ $UFM_NDT_Plugin -eq $total ] && [ $total -ne 0 ]; then
 #     ln -snf ../plugins/UFM_NDT_Plugin/.ci/ci_matrix.yaml matrix_job_ci.yaml
 # else
