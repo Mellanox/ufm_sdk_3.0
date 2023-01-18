6fc612b279b6bb817810989c745fac50f752652a
Amit Gueta <agueta@nvidia.com>
2023-01-18 14:56:04 +0200

    add initial ci

--- .ci/cidemo-init.sh
+++ .ci/cidemo-init.sh
@@ -4,13 +4,16 @@
 cd .ci
 printenv
 env
-export conf_file=.ci/ci_matrix.yaml
+
 #UPDATE softlink according to git changes
-# changed_files=$(git diff --name-only remotes/origin/$ghprbTargetBranch)
-# appliance=$(git diff --name-only remotes/origin/$ghprbTargetBranch |grep ufm_appliance |grep -v .gitmodules |wc -l)
-# total=$(git diff --name-only remotes/origin/$ghprbTargetBranch |grep -v .gitmodules |wc -l)
-# if [ $appliance -eq $total ] && [ $total -ne 0 ]; then
-#     ln -snf matrix_job_appliance_ci.yaml matrix_job_ci.yaml
-# else
-#     ln -snf matrix_job_runci.yaml matrix_job_ci.yaml
-# fi
+
+changed_files=$(git diff --name-only remotes/origin/$ghprbTargetBranch)
+echo $changed_files
+echo "------------------------------"
+UFM_NDT_Plugin=$(git diff --name-only remotes/origin/$ghprbTargetBranch |grep UFM_NDT_Plugin |grep -v .gitmodules |wc -l)
+total=$(git diff --name-only remotes/origin/$ghprbTargetBranch |grep -v .gitmodules |wc -l)
+if [ $UFM_NDT_Plugin -eq $total ] && [ $total -ne 0 ]; then
+    ln -snf ../plugins/UFM_NDT_Plugin/.ci/ci_matrix.yaml matrix_job_ci.yaml
+else
+    ln -snf ci_matrix.yaml matrix_job_ci.yaml
+fi
