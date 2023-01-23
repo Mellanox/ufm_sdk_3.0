2549594506e704342dc75ea4d11163e12eb999d7
Alexander Tolikin <89789216+atolikin94@users.noreply.github.com>
2023-01-23 17:05:55 +0100

    Renaming

--- .ci/cidemo-init.sh
+++ .ci/cidemo-init.sh
@@ -10,7 +10,7 @@ env
 changed_files=$(git diff --name-only remotes/origin/$ghprbTargetBranch)
 echo "------------------------------"
 UFM_NDT_Plugin=$(git diff --name-only remotes/origin/$ghprbTargetBranch | grep UFM_NDT_Plugin |grep -v .gitmodules |wc -l)
-snmp_plugin=$(git diff --name-only remotes/origin/$ghprbTargetBranch | grep snmp_plugin |grep -v .gitmodules |wc -l)
+snmp_plugin=$(git diff --name-only remotes/origin/$ghprbTargetBranch | grep snmp_receiver_plugin |grep -v .gitmodules |wc -l)
 total=$(git diff --name-only remotes/origin/$ghprbTargetBranch |grep -v .gitmodules |wc -l)
 if [ $UFM_NDT_Plugin -eq $total ] && [ $total -ne 0 ]; then
     ln -snf ../plugins/UFM_NDT_Plugin/.ci/ci_matrix.yaml matrix_job_ci.yaml
