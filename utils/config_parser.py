188673097526b31ca4bdeb64dcbef32d61d3e0ec
ananalaghbar <79898567+ananalaghbar@users.noreply.github.com>
2023-02-14 16:40:11 +0200

    Update config_parser.py
    without this fix, the plugins that are running without inline args will be broken

--- utils/config_parser.py
+++ utils/config_parser.py
@@ -47,7 +47,7 @@ class ConfigParser(object):
     def __init__(self, args = None,
                  read_sdk_config = True):
         if not args:
-            ArgsParser.parse_args("UFM SDK", [])
+            args = ArgsParser.parse_args("UFM SDK", [])
         self.sdk_config = configparser.RawConfigParser()
         if read_sdk_config:
             self.sdk_config.read(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),SDK_CONFIG_FILE))
