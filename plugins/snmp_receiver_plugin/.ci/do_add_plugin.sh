a633c487f58b851274808bd8e50a5772718aaa8f
Alexander Tolikin <atolikin@nvidia.com>
2023-02-16 17:25:16 +0200

    Forcing UFM to run here and to use https

--- plugins/snmp_receiver_plugin/.ci/do_add_plugin.sh
+++ plugins/snmp_receiver_plugin/.ci/do_add_plugin.sh
@@ -9,6 +9,17 @@ send -- "enable\r"
 expect "# "
 send -- "config terminal\r"
 expect "/(config/) # "
+send -- "show ufm status\r"
+expect "/(config/) # "
+sleep 10
+send -- "ufm web-client mode https\r"
+expect "/(config/) # "
+send -- "ufm start\r"
+expect "/(config/) # "
+sleep 20
+send -- "ufm ha takeover\r"
+expect "/(config/) # "
+sleep 20
 send -- "ufm plugin snmp remove\r"
 expect "/(config/) # "
 sleep 10
