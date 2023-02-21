b76deacd2cdda925878f0fa1abce7a6f1f1e093a
Elad Gershon <egershon@nvidia.com>
2023-02-21 19:44:02 +0200

    Plugin Sysinfo second build with image and Comments
    Signed-off-by: Elad Gershon <egershon@nvidia.com>

---
+++ plugins/sysinfo_plugin/build/config/init.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Copyright (C) Mellanox Technologies Ltd. 2001-2021.   ALL RIGHTS RESERVED.
+#
+# This software product is a proprietary product of Mellanox Technologies Ltd.
+# (the "Company") and all right, title, and interest in and to the software product,
+# including all associated intellectual property rights, are and shall
+# remain exclusively with the Company.
+#
+# This software product is governed by the End User License Agreement
+# provided with the software product.
+
+# ================================================================
+# This script prepares and checks sysinfo docker container Environment
+# ================================================================
+
+set -eE
+
+# Updating /config folder
+#mv /opt/ufm/ufm_plugin_sysinfo/sysinfo.conf /config
+#mv /opt/ufm/ufm_plugin_sysinfo/sysinfo_httpd_proxy.conf /config
+#mv /opt/ufm/ufm_plugin_sysinfo/ufm_plugin_sysinfo_httpd.conf /config
+cp -f /opt/ufm/ufm_plugin_sysinfo/*.conf /config
+
+echo /opt/ufm/files/log:/log > /config/sysinfo_shared_volumes.conf
+
+exit 0
---
+++ plugins/sysinfo_plugin/tests/wrapper_test.py
@@ -0,0 +1,67 @@
+from datetime import datetime, timedelta
+import subprocess
+
+HOST_IP = "r-ufm247"
+DEFAULT_PASSWORD = "123456"
+
+# resources
+NDTS = "list"
+UPLOAD = "upload"
+COMPARE = "compare"
+DELETE = "delete"
+CANCEL = "cancel"
+REPORTS = "reports"
+REPORT_ID_PATTERN = "^reports/\d+$"
+
+# authentication types
+BASIC = "basic"
+CLIENT = "client"
+TOKEN = "token"
+
+
+def assert_equal(test_name, output):
+    if "response code: 200" in output.decode('utf-8'):
+        print("Test {}: PASS".format(test_name))
+    else:
+        print("Test {}: FAIL (output: {})".format(test_name, output))
+
+
+def main():
+    wrapper_file = "ndt_rest_api.py"
+    authentication_cmd = [BASIC, "-u", "admin", "-p", "123456"]
+
+    cmd = ["python3", wrapper_file, HOST_IP, NDTS] + authentication_cmd
+    assert_equal(NDTS, subprocess.check_output(cmd))
+
+    ndts_folder = "positive_flow_ndts"
+    files = "{}/switch_to_host_topo1.ndt|switch_to_host,{}/switch_to_switch_topo2.ndt|switch_to_switch" \
+        .format(ndts_folder, ndts_folder)
+    cmd = ["python3", wrapper_file, HOST_IP, UPLOAD, "-uf", files] + authentication_cmd
+    assert_equal(UPLOAD, subprocess.check_output(cmd))
+
+    cmd = ["python3", wrapper_file, HOST_IP, COMPARE] + authentication_cmd
+    assert_equal(COMPARE, subprocess.check_output(cmd))
+
+    cmd = ["python3", wrapper_file, HOST_IP, REPORTS] + authentication_cmd
+    assert_equal(REPORTS, subprocess.check_output(cmd))
+
+    report_id = "1"
+    cmd = ["python3", wrapper_file, HOST_IP, REPORTS, "-id", report_id] + authentication_cmd
+    assert_equal(REPORTS + "/{}".format(report_id), subprocess.check_output(cmd))
+
+    start = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    end = (datetime.now() + timedelta(minutes=5)).strftime("%Y-%m-%d %H:%M:%S")
+    interval = "5"
+    cmd = ["python3", wrapper_file, HOST_IP, COMPARE, "-s", start, "-e", end, "-i", interval] + authentication_cmd
+    assert_equal("periodic " + COMPARE, subprocess.check_output(cmd))
+
+    cmd = ["python3", wrapper_file, HOST_IP, CANCEL] + authentication_cmd
+    assert_equal(CANCEL, subprocess.check_output(cmd))
+
+    files = "switch_to_host_topo1.ndt,switch_to_switch_topo2.ndt"
+    cmd = ["python3", wrapper_file, HOST_IP, DELETE, "-df", files] + authentication_cmd
+    assert_equal(DELETE, subprocess.check_output(cmd))
+
+
+if __name__ == "__main__":
+    main()
