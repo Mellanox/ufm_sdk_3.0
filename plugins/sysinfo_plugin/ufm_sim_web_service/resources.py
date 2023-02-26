f59f1b51308fb3ae2edcdbbb3b4ec86524586b5f
Elad Gershon <egershon@nvidia.com>
2023-02-26 07:40:41 +0200

    fix comments issues
    Signed-off-by: Elad Gershon <egershon@nvidia.com>

--- plugins/sysinfo_plugin/ufm_sim_web_service/resources.py
+++ plugins/sysinfo_plugin/ufm_sim_web_service/resources.py
@@ -1,18 +1,16 @@
-"""
-@copyright:
-    Copyright (C) Mellanox Technologies Ltd. 2021. ALL RIGHTS RESERVED.
-
-    This software product is a proprietary product of Mellanox Technologies
-    Ltd. (the "Company") and all right, title, and interest in and to the
-    software product, including all associated intellectual property rights,
-    are and shall remain exclusively with the Company.
-
-    This software product is governed by the End User License Agreement
-    provided with the software product.
-
-@author: Nahum Kilim
-@date:   September 20, 2021
-"""
+#
+# Copyright (C) 2013-2023 NVIDIA CORPORATION & AFFILIATES. ALL RIGHTS RESERVED.
+#
+# This software product is a proprietary product of Nvidia Corporation and its affiliates
+# (the "Company") and all right, title, and interest in and to the software
+# product, including all associated intellectual property rights, are and
+# shall remain exclusively with the Company.
+#
+# This software product is governed by the End User License Agreement
+# provided with the software product.
+# author: Elad Gershon
+# date:   February 1, 2023
+#
 
 import configparser
 import json
