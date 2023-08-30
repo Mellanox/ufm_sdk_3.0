e37e4c613af2f7bda6bab87da3d9e7cc9446e230
rdomnitz <rdomnitz@nvidia.com>
2023-08-30 14:14:52 +0300

    real fubric

---
+++ plugins/UFM_NDT_Plugin/.ci/real_fabric.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+export SERVER_HOST=$SERVER_HOST
+expect << EOF
+spawn ssh admin@${SERVER_HOST}
+expect "Password:*"
+send -- "admin\r"
+expect "> "
+send -- "enable\r"
+expect "# "
+send -- "config terminal\r"
+expect "/(config/) # "
+send -- "no ufm start\r"
+expect "/(config/) # "
+send -- "_shell\r"
+exit 0
+EOF
