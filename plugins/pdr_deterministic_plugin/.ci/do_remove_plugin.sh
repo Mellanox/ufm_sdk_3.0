1f84fb30731bd175ccefea7968d2abe2fbc8c6ac
shahar spormas <sspormas@nvidia.com>
2024-01-21 11:16:08 +0200

    Update do_remove_plugin.sh

--- plugins/pdr_deterministic_plugin/.ci/do_remove_plugin.sh
+++ plugins/pdr_deterministic_plugin/.ci/do_remove_plugin.sh
@@ -3,7 +3,7 @@ export SERVER_HOST=$SERVER_HOST
 expect << EOF
 spawn ssh -o PasswordAuthentication=yes -o KexAlgorithms=+diffie-hellman-group14-sha1 admin@${SERVER_HOST}
 expect "*\])?"
-send --"yes\r"
+send -- "yes\r"
 expect "Password:*"
 send -- "admin\r"
 expect "> "
