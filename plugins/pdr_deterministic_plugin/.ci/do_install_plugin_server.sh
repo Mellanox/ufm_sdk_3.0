3af73614e0c4f55b22f843787733e4f1d130ca46
shahar spormas <sspormas@nvidia.com>
2024-01-22 11:12:04 +0200

    Update do_install_plugin_server.sh
    remove fingerprint expect

--- plugins/pdr_deterministic_plugin/.ci/do_install_plugin_server.sh
+++ plugins/pdr_deterministic_plugin/.ci/do_install_plugin_server.sh
@@ -4,8 +4,6 @@ export SERVER_HOST=$SERVER_HOST
 export PASSWORD=$PASSWORD
 expect << EOF
 spawn ssh -o PasswordAuthentication=yes -o KexAlgorithms=+diffie-hellman-group14-sha1 admin@${SERVER_HOST}
-expect "*\])?"
-send -- "yes\r"
 expect "Password:*"
 send -- "admin\r"
 expect "> "
