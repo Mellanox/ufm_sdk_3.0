96386f0375a5156f9a8e4da166f78832dc575b60
Elad Gershon <egershon@nvidia.com>
2024-01-23 17:00:38 +0200

    change the location of the sleep to make it work
    Signed-off-by: Elad Gershon <egershon@nvidia.com>

--- plugins/pdr_deterministic_plugin/.ci/do_run_test.sh
+++ plugins/pdr_deterministic_plugin/.ci/do_run_test.sh
@@ -18,7 +18,7 @@ expect "# "
 send -- "/opt/ufm/scripts/manage_ufm_plugins.sh enable -p pdr_deterministic\r"
 expect "# "
 send -- "python3 /tmp/simulation_telemetry.py\r"
-sleep 60
 expect "# "
+sleep 60
 send -- "echo $?"
 EOF
