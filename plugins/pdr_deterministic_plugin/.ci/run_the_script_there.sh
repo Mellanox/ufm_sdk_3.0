780d1d166e61032bde9865edd27b4eafd4b07c7d
egershonNvidia <108881287+egershonNvidia@users.noreply.github.com>
2024-02-08 17:54:49 +0200

    added kill simulation if exists

--- plugins/pdr_deterministic_plugin/.ci/run_the_script_there.sh
+++ plugins/pdr_deterministic_plugin/.ci/run_the_script_there.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
-echo "I am printing"
+pid=$(ps -fade | grep simulation_telemetry | egrep -v grep | awk '// { print $2 }')
+if [[ -z $pid ]]; then kill -9 $pid;fi
 python3 /tmp/simulation_telemetry.py
 result=$?
 exit result
