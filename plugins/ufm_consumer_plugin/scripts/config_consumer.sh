ec83e387e8a31372eaa8a710b710131776659d65
Alex Tabachnik <atabachnik@nvidia.com>
2024-02-11 13:22:59 +0200

    issue:3772234 Make consumer configuration percistent
    Signed-off-by: Alex Tabachnik <atabachnik@nvidia.com>

--- plugins/ufm_consumer_plugin/scripts/config_consumer.sh
+++ plugins/ufm_consumer_plugin/scripts/config_consumer.sh
@@ -4,6 +4,32 @@
 # values for multisubnet_enabled = true and 
 # multisubnet_role = consumer
 . /opt/ufm/scripts/common
+
+keep_config_file()
+{
+    conf_file_path=$1
+    conf_file_name=$(basename $conf_file_path)
+    target_dir=/config
+    target_file_path="${target_dir}/${conf_file_name}"
+
+    if [ ! -f ${target_file_path} ]; then
+        if [ -f ${conf_file_path} ]; then
+            mv ${conf_file_path} ${target_dir}
+            ln -s ${target_file_path} ${conf_file_path}
+        else
+            echo "UFM file ${conf_file_path} not found"
+            exit 1
+        fi
+    else # file exist, but need to check if link exist
+        if [ -f ${conf_file_path} ] && [ ! -L ${conf_file_path} ]; then
+            # The file is not a symbolic link
+            rm -f ${conf_file_path}
+            ln -s ${target_file_path} ${conf_file_path}
+        fi
+    fi
+    chown -R ufmapp:ufmapp ${target_file_path} ${conf_file_path}
+}
+
 #
 UpdCfg /opt/ufm/files/conf/gv.cfg Multisubnet multisubnet_enabled true
 #
@@ -32,3 +58,16 @@ sed -i -e "s/Listen 443/Listen $ssl_apache_port/g" -e "s/Listen 80/Listen $apach
 sed -i "s/VirtualHost _default_:443/VirtualHost _default_:$ssl_apache_port/g" /etc/apache2/sites-available/default-ssl.conf
 sed -i "s/VirtualHost \*:80/VirtualHost \*:$apache_port/g" /etc/apache2/sites-available/000-default.conf
 sed -i "s/APACHE_PORT/$ssl_apache_port/g" /config/ufm_consumer_ui_conf.json
+
+# keep config file for restart - save all the provider info in gv.cfg
+# TODO: sqlite does not work of some reason with db file defined as synbolic link
+# need to investigate: 
+# 1. If UFM consumer should keep some data in database to be persistent
+# 2. If yes - how to manage sqlight to work with db which is link pointing to another file
+#for conf_file2keep in /opt/ufm/files/conf/gv.cfg /opt/ufm/files/sqlite/gv.db /opt/ufm/files/conf/ufm_providers_credentials.cfg;
+[ ! -f /opt/ufm/files/conf/ufm_providers_credentials.cfg ] &&  echo "[Credentials]" > /opt/ufm/files/conf/ufm_providers_credentials.cfg 
+for conf_file2keep in /opt/ufm/files/conf/gv.cfg /opt/ufm/files/conf/ufm_providers_credentials.cfg;
+    do
+         keep_config_file $conf_file2keep
+    done
+exit 0
--- plugins/ufm_consumer_plugin/scripts/start_ufm.sh
+++ plugins/ufm_consumer_plugin/scripts/start_ufm.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+# configure consumer UFM
 ./config_consumer.sh
+status=$?
+if [ $status -ne 0 ]; then
+    echo "Failed to config UFM. UFM consumer plugin will not start."
+    exit 1
+fi
+# start UFM
 /etc/init.d/ufmd start
+exit 0
+
