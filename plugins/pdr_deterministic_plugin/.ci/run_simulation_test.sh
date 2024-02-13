5b5d0f2d22f16bcf016b18468552f687225bdc7a
Elad Gershon <egershon@nvidia.com>
2024-02-13 16:22:52 +0200

    removed openmd command
    Signed-off-by: Elad Gershon <egershon@nvidia.com>

--- plugins/pdr_deterministic_plugin/.ci/run_simulation_test.sh
+++ plugins/pdr_deterministic_plugin/.ci/run_simulation_test.sh
@@ -11,9 +11,7 @@ cp /opt/ufm/conf/plugins/pdr_deterministic/pdr_deterministic.conf /config
 docker run --network=host -v /config:/config mellanox/ufm-plugin-pdr_deterministic >/tmp/pdr_deterministic_plugin.log 2>&1&
 sleep 5
 container_pid=$(docker ps| grep pdr_ | egrep -v grep | awk '// { print $1 }')
-echo $container_pid
-docker logs $container_pid >& /tmp/pdr_deterministic_plugin.log&
-python3 /tmp/simulation_telemetry.py 1>&2
+python3 /tmp/simulation_telemetry.py > /tmp/log_of_simulation.log
 result=$?
 docker stop $container_pid
 exit $result
