8ae081a9f97817b268a2ad0f29317ba390d20f96
egershonNvidia <108881287+egershonNvidia@users.noreply.github.com>
2024-04-11 18:26:46 +0300

    Update run_simulation_test.sh

--- plugins/pdr_deterministic_plugin/.ci/run_simulation_test.sh
+++ plugins/pdr_deterministic_plugin/.ci/run_simulation_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
+sed -i -e 's/\nTEST_MODE=True\n//g' /opt/ufm/files/conf/plugins/pdr_deterministic/pdr_deterministic.conf
 echo -e '\n[Common]\nTEST_MODE=True\n'>>/opt/ufm/files/conf/plugins/pdr_deterministic/pdr_deterministic.conf
 sed -i -e 's/DRY_RUN=False/DRY_RUN=True/g' /opt/ufm/files/conf/plugins/pdr_deterministic/pdr_deterministic.conf
 sed -i -e 's/INTERVAL=300/INTERVAL=10/g' /opt/ufm/files/conf/plugins/pdr_deterministic/pdr_deterministic.conf
@@ -11,6 +12,7 @@ cp /opt/ufm/conf/plugins/pdr_deterministic/pdr_deterministic.conf /config
 docker run --network=host -v /config:/config mellanox/ufm-plugin-pdr_deterministic >/tmp/pdr_deterministic_plugin.log 2>&1&
 sleep 5
 container_pid=$(docker ps| grep pdr_ | egrep -v grep | awk '// { print $1 }')
+kill -9 $(lsof -t -i:9090)
 python3 /tmp/simulation_telemetry.py > /tmp/log_of_simulation.log
 result=$?
 docker stop $container_pid
