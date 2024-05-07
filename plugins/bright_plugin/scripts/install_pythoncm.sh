370a726d3e9bc5ab1a4ccedbd206eabb4bfe3718
ananalaghbar <anana@nvidia.com>
2024-05-07 11:29:30 +0100

    issue:3621850: Fixing Bright plugin - High Vulnerability Severity

--- plugins/bright_plugin/scripts/install_pythoncm.sh
+++ plugins/bright_plugin/scripts/install_pythoncm.sh
@@ -4,7 +4,7 @@ set -eE
 
 # install bright python API
 SRC_DIR_PATH=/opt/ufm/ufm_plugin_bright/bright_plugin
-PYTHON_PACKAGES_PATH=$(python3.9 -c 'import site; print(site.getsitepackages()[0])')
+PYTHON_PACKAGES_PATH=$(python3 -c 'import site; print(site.getsitepackages()[0])')
 mv $SRC_DIR_PATH/src/bright/pythoncm.tar.gz ${PYTHON_PACKAGES_PATH}
 tar -xf ${PYTHON_PACKAGES_PATH}/pythoncm.tar.gz -C ${PYTHON_PACKAGES_PATH}
 rm -rf ${PYTHON_PACKAGES_PATH}/pythoncm.tar.gz
