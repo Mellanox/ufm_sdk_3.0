f04f7d995f514c83b7817b2bb87f5f2bc9b03f6b
vg12345 <77556137+vg12345@users.noreply.github.com>
2024-05-08 14:29:41 +0300

    Issue:3874733:Fixes for blacklist functionality in PDR plugin (#208)

--- plugins/pdr_deterministic_plugin/ufm_sim_web_service/api/pdr_plugin_api.py
+++ plugins/pdr_deterministic_plugin/ufm_sim_web_service/api/pdr_plugin_api.py
@@ -10,11 +10,14 @@
 # provided with the software product.
 #
 
-import re
 import time
-
-from flask import request
+from http import HTTPStatus
+from json import JSONDecodeError
+from flask import json, request
 from utils.flask_server.base_flask_api_server import BaseAPIApplication
+
+ERROR_INCORRECT_INPUT_FORMAT = "Incorrect input format"
+EOL = '\n'
 
 class PDRPluginAPI(BaseAPIApplication):
     '''
@@ -46,51 +49,60 @@ class PDRPluginAPI(BaseAPIApplication):
         """
         items = self.isolation_mgr.exclude_list.items()
         formatted_items = [f"{item.port_name}: {'infinite' if item.ttl_seconds == 0 else int(max(0, item.remove_time - time.time()))}" for item in items]
-        return '\n'.join(formatted_items) + ('' if not formatted_items else '\n')
+        response = EOL.join(formatted_items) + ('' if not formatted_items else EOL)
+        return response, HTTPStatus.OK
 
 
     def exclude_ports(self):
         """
         Parse input ports and add them to exclude list (or just update TTL)
-        Input string example: 0c42a10300756a04_1,98039b03006c73ba_2:300
+        Input string example: [["0c42a10300756a04_1"],["98039b03006c73ba_2",300]]
         TTL that follows port name after the colon is optional
         """
-        data_dict = request.form.to_dict()
-        if not data_dict:
-            return "No ports added to exclude list"
-      
-        response = ""
-        excluded_pors_str = next(iter(data_dict.keys()))
-        pair_pattern = re.compile(r'\((.*?)(?:,(.*?))?\)')
-        pairs = pair_pattern.findall(excluded_pors_str)
-        for pair in pairs:
-            if pair[0].strip():
-                port_name = pair[0].strip()
-                ttl = 0 if not pair[1].strip().isdigit() else int(pair[1].strip())
-                self.isolation_mgr.exclude_list.add(port_name, ttl)
-                response += f"Port {port_name} added to exclude list\n"
 
-        return response
+        try:
+            pairs = self.get_request_data()
+        except JSONDecodeError:
+            return ERROR_INCORRECT_INPUT_FORMAT + EOL, HTTPStatus.BAD_REQUEST
+        
+        response = ""
+        for pair in pairs:
+            if pair:
+                port_name = pair[0]
+                ttl = 0 if len(pair) == 1 else int(pair[1])
+                self.isolation_mgr.exclude_list.add(port_name, ttl)
+                response += f"Port {port_name} added to exclude list{EOL}"
+
+        return response, HTTPStatus.OK
 
 
     def include_ports(self):
         """
         Remove ports from exclude list
-        Input string: comma separated port names
+        Input string: comma separated port names list
+        Example: ["0c42a10300756a04_1","98039b03006c73ba_2"]
         """
-        data_dict = request.form.to_dict()
-        if not data_dict:
-            return "No ports removed from exclude list"
-      
-        port_names_str = next(iter(data_dict.keys()))
-        port_names = re.findall(r'\w+', port_names_str)
+        try:
+            port_names = self.get_request_data()
+        except JSONDecodeError:
+            return ERROR_INCORRECT_INPUT_FORMAT + EOL, HTTPStatus.BAD_REQUEST
+
         response = ""
         for port_name in port_names:
-            if port_name.strip():
-                port_name = port_name.strip();
-                if self.isolation_mgr.exclude_list.remove(port_name):
-                    response += f"Port {port_name} removed from exclude list\n"
-                else:
-                    response += f"Port {port_name} is not in exclude list\n"
-        
-        return response
+            if self.isolation_mgr.exclude_list.remove(port_name):
+                response += f"Port {port_name} removed from exclude list{EOL}"
+            else:
+                response += f"Port {port_name} is not in exclude list{EOL}"
+
+        return response, HTTPStatus.OK
+    
+    def get_request_data(self):
+        """
+        Deserialize request json data into object
+        """
+        if request.is_json:
+            # Directly convert JSON data into Python object
+            return request.get_json()
+        else:
+            # Attempt to load plain data text as JSON
+            return json.loads(request.get_data(as_text=True))
