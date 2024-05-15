6c61686bf317f091e5b19ae17ba86438a3c47a8a
Vitaly Gavensky <vgavensky@nvidia.com>
2024-05-15 16:13:15 +0300

    First simple pytest test

---
+++ plugins/pdr_deterministic_plugin/tests/exclude_list_tests.py
@@ -0,0 +1,26 @@
+#
+# Copyright © 2013-2024 NVIDIA CORPORATION & AFFILIATES. ALL RIGHTS RESERVED.
+#
+# This software product is a proprietary product of Nvidia Corporation and its affiliates
+# (the "Company") and all right, title, and interest in and to the software
+# product, including all associated intellectual property rights, are and
+# shall remain exclusively with the Company.
+#
+# This software product is governed by the End User License Agreement
+# provided with the software product.
+#
+
+#import pytest
+from plugins.pdr_deterministic_plugin.ufm_sim_web_service.constants import PDRConstants as Constants
+from plugins.pdr_deterministic_plugin.ufm_sim_web_service.exclude_list import ExcludeList
+from plugins.pdr_deterministic_plugin.ufm_sim_web_service.isolation_algo import create_logger
+
+def test_get_from_empty_list():
+    """
+    Create exclude list and ensure its empty via its method
+    """
+
+    logger = create_logger(Constants.LOG_FILE)
+    exclude_list = ExcludeList(logger)
+    items = exclude_list.items()
+    assert not items
