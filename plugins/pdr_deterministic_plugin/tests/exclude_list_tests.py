e6cd666a12329a1994282e8fc506099eb7a75d3c
Vitaly Gavensky <vgavensky@nvidia.com>
2024-05-16 19:53:03 +0300

    Experiment with standalone plugin

--- plugins/pdr_deterministic_plugin/tests/exclude_list_tests.py
+++ plugins/pdr_deterministic_plugin/tests/exclude_list_tests.py
@@ -21,7 +21,7 @@ from constants import PDRConstants as Constants
 from exclude_list import ExcludeList, ExcludeListItem
 from isolation_algo import create_logger
 
-# Temp
+# Temp 1
 
 def get_logger():
     """
