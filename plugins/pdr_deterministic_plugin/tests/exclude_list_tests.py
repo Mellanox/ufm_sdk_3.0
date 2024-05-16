19aba8fdd299d8d1dc57e0384e99a886962a3f1a
Vitaly Gavensky <vgavensky@nvidia.com>
2024-05-16 14:11:29 +0300

    Test ExcludeList class methods directly

--- plugins/pdr_deterministic_plugin/tests/exclude_list_tests.py
+++ plugins/pdr_deterministic_plugin/tests/exclude_list_tests.py
@@ -12,25 +12,80 @@
 
 import os
 import tempfile
-
-#import pytest
+import time
+import pytest
 from constants import PDRConstants as Constants
-from exclude_list import ExcludeList
+from exclude_list import ExcludeList, ExcludeListItem
 from isolation_algo import create_logger
 
-def test_get_from_empty_exclude_list():
+def get_logger():
+    """
+    Return logger associated with log file in temporary directory
+    """
+    log_name = os.path.basename(Constants.LOG_FILE)
+    log_path = os.path.join(tempfile.gettempdir(), log_name)
+    return create_logger(log_path)
+
+pytest.mark.run(order=0)
+def test_exclude_list_class_methods():
     """
     Create exclude list and ensure its empty via its method
     """
-    print("Test 5")
+    # Create exclude list and ensure it's
 
-    log_name = os.path.basename(Constants.LOG_FILE)
-    log_path = os.path.join(tempfile.gettempdir(), log_name)
-
-    logger = create_logger(log_path)
-    exclude_list = ExcludeList(logger)
+    exclude_list = ExcludeList(get_logger())
     items = exclude_list.items()
     assert not items
 
+    # Add ports to excluded list
+    excluded_ports = [
+        ExcludeListItem("0123456789aaabbb_1", 0),  # Add forever
+        ExcludeListItem("9876543210cccddd_2", 30), # Add for 30 seconds
+        ExcludeListItem("3456789012eeefff_3", 0)   # Add forever
+    ]
+    for port in excluded_ports:
+        exclude_list.add(port.port_name, port.ttl_seconds)
+
+    # Test excluded list size
+    items = exclude_list.items()
+    assert items and len(items) == len(excluded_ports)
+
+    # Test 'contains' method
+    for port in excluded_ports:
+        assert exclude_list.contains(port.port_name)
+
+    # Test excluded list content
+    for (index, item) in enumerate(items):
+        assert item.port_name == excluded_ports[index].port_name
+        assert item.ttl_seconds == excluded_ports[index].ttl_seconds
+
+    # Test auto-remove of second port after TTL is expired
+    auto_remove_port = excluded_ports[1]
+    time.sleep(auto_remove_port.ttl_seconds + 1)
+    assert not exclude_list.contains(auto_remove_port.port_name)
+
+    # Test excluded list size
+    items = exclude_list.items()
+    assert items and len(items) == (len(excluded_ports) - 1)
+
+    # Test excluded list content
+    for port in excluded_ports:
+        if port.port_name != auto_remove_port.port_name:
+            assert exclude_list.contains(port.port_name)
+
+    # Test forced remove of third port
+    remove_port = excluded_ports[2]
+    exclude_list.remove(port.port_name)
+    assert not exclude_list.contains(remove_port.port_name)
+
+    # Test excluded list size
+    items = exclude_list.items()
+    assert items and len(items) == (len(excluded_ports) - 2)
+
+    # Test excluded list content
+    for port in excluded_ports:
+        if port.port_name != remove_port.port_name and port.port_name != auto_remove_port.port_name:
+            assert exclude_list.contains(port.port_name)
+
 if __name__ == '__main__':
-    test_get_from_empty_exclude_list()
+    test_exclude_list_class_methods()
