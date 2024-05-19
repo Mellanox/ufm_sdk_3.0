9583d6f604091c790be47ecb0e4657563de1d520
Vitaly Gavensky <vgavensky@nvidia.com>
2024-05-19 17:07:42 +0300

    Temp: full simulation

---
+++ plugins/pdr_deterministic_plugin/.pytest/run_pdr_standalone_pytest.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+#
+PLUGIN_DIR="plugins/pdr_deterministic_plugin"
+pip install -r $PLUGIN_DIR/requirements.txt
+
+cp -r utils $PLUGIN_DIR/ufm_sim_web_service
+cp -r utils $PLUGIN_DIR/tests
+
+echo "Init PDR configuration file"
+CONFIG_FILE="/config/pdr_deterministic.conf"
+mkdir -p /config
+cp -f $PLUGIN_DIR/build/config/pdr_deterministic.conf "$CONFIG_FILE"
+sed -i -e 's/\nTEST_MODE=True\n//g' "$CONFIG_FILE"
+echo -e '\n[Common]\nTEST_MODE=True\n' >> "$CONFIG_FILE"
+sed -i -e 's/DRY_RUN=False/DRY_RUN=True/g' "$CONFIG_FILE"
+sed -i -e 's/INTERVAL=300/INTERVAL=10/g' "$CONFIG_FILE"
+sed -i -e 's/CONFIGURED_TEMP_CHECK=False/CONFIGURED_TEMP_CHECK=True/g' "$CONFIG_FILE"
+sed -i -e 's/DEISOLATE_CONSIDER_TIME=5/DEISOLATE_CONSIDER_TIME=1/g' "$CONFIG_FILE"
+
+echo "Starting standalone PDR process"
+python $PLUGIN_DIR/ufm_sim_web_service/isolation_algo.py &
