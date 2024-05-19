ef28d35a8bbaeef93a87604b4d77ab986bfa2e97
Vitaly Gavensky <vgavensky@nvidia.com>
2024-05-19 16:48:05 +0300

    Temp: full simulation

--- plugins/pdr_deterministic_plugin/.pytest/run_simulation_pytest.sh
+++ plugins/pdr_deterministic_plugin/.pytest/run_simulation_pytest.sh
@@ -4,17 +4,18 @@ PLUGIN_DIR="plugins/pdr_deterministic_plugin"
 pip install -r $PLUGIN_DIR/requirements.txt
 
 start_simulation_server_pytest() {
-    cp -r utils $PLUGIN_DIR/tests
+    # Init configuration file
     CONFIG_FILE="/config/pdr_deterministic.conf"
-    mkdir -p /config
-    cp -f $PLUGIN_DIR/build/config/pdr_deterministic.conf "$CONFIG_FILE"
-    sed -i -e 's/\nTEST_MODE=True\n//g' "$CONFIG_FILE"
-    echo -e '\n[Common]\nTEST_MODE=True\n' >> "$CONFIG_FILE"
-    sed -i -e 's/DRY_RUN=False/DRY_RUN=True/g' "$CONFIG_FILE"
-    sed -i -e 's/INTERVAL=300/INTERVAL=10/g' "$CONFIG_FILE"
-    sed -i -e 's/CONFIGURED_TEMP_CHECK=False/CONFIGURED_TEMP_CHECK=True/g' "$CONFIG_FILE"
-    sed -i -e 's/DEISOLATE_CONSIDER_TIME=5/DEISOLATE_CONSIDER_TIME=1/g' "$CONFIG_FILE"
+    sudo mkdir -p /config
+    sudo cp -f $PLUGIN_DIR/build/config/pdr_deterministic.conf "$CONFIG_FILE"
+    sudo sed -i -e 's/\nTEST_MODE=True\n//g' "$CONFIG_FILE"
+    sudo echo -e '\n[Common]\nTEST_MODE=True\n' >> "$CONFIG_FILE"
+    sudo sed -i -e 's/DRY_RUN=False/DRY_RUN=True/g' "$CONFIG_FILE"
+    sudo sed -i -e 's/INTERVAL=300/INTERVAL=10/g' "$CONFIG_FILE"
+    sudo sed -i -e 's/CONFIGURED_TEMP_CHECK=False/CONFIGURED_TEMP_CHECK=True/g' "$CONFIG_FILE"
+    sudo sed -i -e 's/DEISOLATE_CONSIDER_TIME=5/DEISOLATE_CONSIDER_TIME=1/g' "$CONFIG_FILE"
 
+    cp -r utils $PLUGIN_DIR/tests
     echo "Starting 'isolation_algo.py'"
     python $PLUGIN_DIR/ufm_sim_web_service/isolation_algo.py &
 }
