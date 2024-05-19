f159e1aa91efa994b505342f2982a497f8be5730
Vitaly Gavensky <vgavensky@nvidia.com>
2024-05-19 15:46:47 +0300

    Temp: full simulation

--- plugins/pdr_deterministic_plugin/.pytest/run_simulation_pytest.sh
+++ plugins/pdr_deterministic_plugin/.pytest/run_simulation_pytest.sh
@@ -1,23 +1,32 @@
 #!/bin/bash -x
 
+sudo -s
 PLUGIN_DIR="plugins/pdr_deterministic_plugin"
 pip install -r $PLUGIN_DIR/requirements.txt
-cp -r utils $PLUGIN_DIR/tests
-cp -r utils $PLUGIN_DIR/ufm_sim_web_service
 
-CONFIG_FILE="/config/pdr_deterministic.conf"
-mkdir -p /config
-cp -f $PLUGIN_DIR/build/config/pdr_deterministic.conf "$CONFIG_FILE"
-sed -i -e 's/\nTEST_MODE=True\n//g' "$CONFIG_FILE"
-echo -e '\n[Common]\nTEST_MODE=True\n' >> "$CONFIG_FILE"
-sed -i -e 's/DRY_RUN=False/DRY_RUN=True/g' "$CONFIG_FILE"
-sed -i -e 's/INTERVAL=300/INTERVAL=10/g' "$CONFIG_FILE"
-sed -i -e 's/CONFIGURED_TEMP_CHECK=False/CONFIGURED_TEMP_CHECK=True/g' "$CONFIG_FILE"
-sed -i -e 's/DEISOLATE_CONSIDER_TIME=5/DEISOLATE_CONSIDER_TIME=1/g' "$CONFIG_FILE"
+start_simulation_server_pytest() {
+    cp -r utils $PLUGIN_DIR/tests
+    CONFIG_FILE="/config/pdr_deterministic.conf"
+    mkdir -p /config
+    cp -f $PLUGIN_DIR/build/config/pdr_deterministic.conf "$CONFIG_FILE"
+    sed -i -e 's/\nTEST_MODE=True\n//g' "$CONFIG_FILE"
+    echo -e '\n[Common]\nTEST_MODE=True\n' >> "$CONFIG_FILE"
+    sed -i -e 's/DRY_RUN=False/DRY_RUN=True/g' "$CONFIG_FILE"
+    sed -i -e 's/INTERVAL=300/INTERVAL=10/g' "$CONFIG_FILE"
+    sed -i -e 's/CONFIGURED_TEMP_CHECK=False/CONFIGURED_TEMP_CHECK=True/g' "$CONFIG_FILE"
+    sed -i -e 's/DEISOLATE_CONSIDER_TIME=5/DEISOLATE_CONSIDER_TIME=1/g' "$CONFIG_FILE"
 
-echo "Starting 'isolation_algo.py'"
-python $PLUGIN_DIR/ufm_sim_web_service/isolation_algo.py &
-echo "Starting 'simulation_telemetry.py'"
-python $PLUGIN_DIR/tests/simulation_telemetry.py
-echo "Terminating 'isolation_algo.py'"
-pkill -9 -f isolation_algo.py 2>/dev/null || true
+    echo "Starting 'isolation_algo.py'"
+    python $PLUGIN_DIR/ufm_sim_web_service/isolation_algo.py &
+}
+
+start_pdr_standalone_pytest() {
+    cp -r utils $PLUGIN_DIR/ufm_sim_web_service
+    echo "Starting 'simulation_telemetry.py'"
+    python $PLUGIN_DIR/tests/simulation_telemetry.py
+}
+
+kill_simulation_server_pytest() {
+    echo "Terminating 'isolation_algo.py'"
+    pkill -9 -f isolation_algo.py 2>/dev/null || true
+}
