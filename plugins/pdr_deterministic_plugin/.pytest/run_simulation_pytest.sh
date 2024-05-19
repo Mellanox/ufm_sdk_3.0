bc9d5d4d42eed5645fecafc18ca0e6ba69583c24
Vitaly Gavensky <vgavensky@nvidia.com>
2024-05-19 08:59:14 +0300

    Temp: full simulation

--- plugins/pdr_deterministic_plugin/.pytest/run_simulation_pytest.sh
+++ plugins/pdr_deterministic_plugin/.pytest/run_simulation_pytest.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -x
 
-pip install -r plugins/pdr_deterministic_plugin/requirements.txt
+PLUGIN_DIR="plugins/pdr_deterministic_plugin"
+pip install -r $PLUGIN_DIR/requirements.txt
+cp -r utils $PLUGIN_DIR/ufm_sim_web_service
 
 CONFIG_FILE="/config/pdr_deterministic.conf"
 mkdir -p /config
-cp -f plugins/pdr_deterministic_plugin/build/config/pdr_deterministic.conf "$CONFIG_FILE"
+cp -f $PLUGIN_DIR/build/config/pdr_deterministic.conf "$CONFIG_FILE"
 sed -i -e 's/\nTEST_MODE=True\n//g' "$CONFIG_FILE"
 echo -e '\n[Common]\nTEST_MODE=True\n' >> "$CONFIG_FILE"
 sed -i -e 's/DRY_RUN=False/DRY_RUN=True/g' "$CONFIG_FILE"
@@ -12,4 +14,4 @@ sed -i -e 's/INTERVAL=300/INTERVAL=10/g' "$CONFIG_FILE"
 sed -i -e 's/CONFIGURED_TEMP_CHECK=False/CONFIGURED_TEMP_CHECK=True/g' "$CONFIG_FILE"
 sed -i -e 's/DEISOLATE_CONSIDER_TIME=5/DEISOLATE_CONSIDER_TIME=1/g' "$CONFIG_FILE"
 
-python plugins/pdr_deterministic_plugin/ufm_sim_web_service/isolation_algo.py
+python $PLUGIN_DIR/ufm_sim_web_service/isolation_algo.py
