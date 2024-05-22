d606677c29c3fb83fbeb6c2d6607a84f04ffea90
Vitaly Gavensky <vgavensky@nvidia.com>
2024-05-22 11:40:21 +0300

    CI changes

--- plugins/pdr_deterministic_plugin/.ci/run_simulation_test.sh
+++ plugins/pdr_deterministic_plugin/.ci/run_simulation_test.sh
@@ -1,14 +1,26 @@
 #!/bin/bash -x
-sed -i -e 's/\nTEST_MODE=True\n//g' /opt/ufm/files/conf/plugins/pdr_deterministic/pdr_deterministic.conf
-echo -e '\n[Common]\nTEST_MODE=True\n'>>/opt/ufm/files/conf/plugins/pdr_deterministic/pdr_deterministic.conf
-sed -i -e 's/DRY_RUN=False/DRY_RUN=True/g' /opt/ufm/files/conf/plugins/pdr_deterministic/pdr_deterministic.conf
-sed -i -e 's/INTERVAL=300/INTERVAL=10/g' /opt/ufm/files/conf/plugins/pdr_deterministic/pdr_deterministic.conf
-sed -i -e 's/CONFIGURED_TEMP_CHECK=False/CONFIGURED_TEMP_CHECK=True/g' /opt/ufm/files/conf/plugins/pdr_deterministic/pdr_deterministic.conf
-sed -i -e 's/DEISOLATE_CONSIDER_TIME=5/DEISOLATE_CONSIDER_TIME=1/g' /opt/ufm/files/conf/plugins/pdr_deterministic/pdr_deterministic.conf
+
+# sed -i -e 's/DRY_RUN=False/DRY_RUN=True/g' /opt/ufm/files/conf/plugins/pdr_deterministic/pdr_deterministic.conf
+# sed -i -e 's/INTERVAL=300/INTERVAL=10/g' /opt/ufm/files/conf/plugins/pdr_deterministic/pdr_deterministic.conf
+# sed -i -e 's/CONFIGURED_TEMP_CHECK=False/CONFIGURED_TEMP_CHECK=True/g' /opt/ufm/files/conf/plugins/pdr_deterministic/pdr_deterministic.conf
+# sed -i -e 's/LINK_DOWN_ISOLATION=False/LINK_DOWN_ISOLATION=True/g' /opt/ufm/files/conf/plugins/pdr_deterministic/pdr_deterministic.conf
+# sed -i -e 's/DEISOLATE_CONSIDER_TIME=5/DEISOLATE_CONSIDER_TIME=1/g' /opt/ufm/files/conf/plugins/pdr_deterministic/pdr_deterministic.conf
+
+# # Remove any existing TEST_MODE lines from the file
+# sed -i -e '/TEST_MODE=\(True\|False\)/d' "$CONFIG_FILE"
+
+# # Check if the section [Common] exists
+# if grep -q '^\[Common\]' "$CONFIG_FILE"; then
+#     # If section exists, insert TEST_MODE=True under it
+#     awk '/^\[Common\]/ {print; print "TEST_MODE=True"; next}1' "$CONFIG_FILE" > temp && mv temp "$CONFIG_FILE"
+# else
+#     # If section does not exist, add it to the file
+#     echo -e '\n[Common]\nTEST_MODE=True\n' >> "$CONFIG_FILE"
+# fi
 
 pid=$(ps -fade | grep /tmp/simulation_telemetry.py | egrep -v grep | awk '// { print $2 }')
 if [[ ! -z $pid ]]; then kill -9 $pid;fi
-cp /opt/ufm/conf/plugins/pdr_deterministic/pdr_deterministic.conf /config
+cp /opt/ufm/files/conf/plugins/pdr_deterministic/pdr_deterministic.conf /config
 docker run --network=host -v /config:/config mellanox/ufm-plugin-pdr_deterministic >/tmp/pdr_deterministic_plugin.log 2>&1&
 sleep 5
 container_pid=$(docker ps| grep pdr_ | egrep -v grep | awk '// { print $1 }')
---
+++ plugins/pdr_deterministic_plugin/.ci/set_simulation_test_config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+
+CONFIG_FILE="/opt/ufm/files/conf/plugins/pdr_deterministic/pdr_deterministic.conf"
+
+sed -i -e 's/DRY_RUN=False/DRY_RUN=True/g' "$CONFIG_FILE"
+sed -i -e 's/INTERVAL=300/INTERVAL=10/g' "$CONFIG_FILE"
+sed -i -e 's/CONFIGURED_TEMP_CHECK=False/CONFIGURED_TEMP_CHECK=True/g' "$CONFIG_FILE"
+sed -i -e 's/LINK_DOWN_ISOLATION=False/LINK_DOWN_ISOLATION=True/g' "$CONFIG_FILE"
+sed -i -e 's/DEISOLATE_CONSIDER_TIME=5/DEISOLATE_CONSIDER_TIME=1/g' "$CONFIG_FILE"
+
+# Remove any existing TEST_MODE lines from the file
+sed -i -e '/TEST_MODE=\(True\|False\)/d' "$CONFIG_FILE"
+
+# Check if the section [Common] exists
+if grep -q '^\[Common\]' "$CONFIG_FILE"; then
+    # If section exists, insert TEST_MODE=True under it
+    awk '/^\[Common\]/ {print; print "TEST_MODE=True"; next}1' "$CONFIG_FILE" > temp && mv temp "$CONFIG_FILE"
+else
+    # If section does not exist, add it to the file
+    echo -e '\n[Common]\nTEST_MODE=True\n' >> "$CONFIG_FILE"
+fi
