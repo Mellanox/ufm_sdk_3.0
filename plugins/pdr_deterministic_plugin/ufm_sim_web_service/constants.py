bfcf471058161725f5d9a1eaaf79c694f88b8885
Vitaly Gavensky <vgavensky@nvidia.com>
2024-06-26 14:01:52 +0300

    Refactoring of isolated ports data structures

--- plugins/pdr_deterministic_plugin/ufm_sim_web_service/constants.py
+++ plugins/pdr_deterministic_plugin/ufm_sim_web_service/constants.py
@@ -107,9 +107,5 @@ class PDRConstants(object):
     ISSUE_INIT = "init"
     ISSUE_LINK_DOWN = "link_down"
 
-    STATE_NORMAL = "normal"
-    STATE_ISOLATED = "isolated"
-    STATE_TREATED = "treated"
-
     # intervals in seconds for testing ber values and corresponding thresholds
     BER_THRESHOLDS_INTERVALS = [(125 * 60, 3), (12 * 60, 2.88)]
