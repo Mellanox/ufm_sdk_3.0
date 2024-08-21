530f6c3d97840085c54bf0792c914c9dde43f3fd
ananalaghbar <anana@nvidia.com>
2024-08-21 16:31:16 +0100

    issue:4040970: [UFM Events Grafana Dashboard Plugin]: Improve the logs parsing regex to handle the anomalous events
    - Fix anomalous events, and support multilines log entry
    - Expose the port_guid,port_num,link_port_guid, link_port_num for the Link events

--- plugins/ufm_events_grafana_dashboard_plugin/conf/fluentd/parse_object_id.rb
+++ plugins/ufm_events_grafana_dashboard_plugin/conf/fluentd/parse_object_id.rb
@@ -4,12 +4,12 @@ def filter(tag, time, record)
   case type
   when 'IBPort', 'Computer'
     # Match the object_id against the regex pattern
-    match = /(?:Computer|Switch): (?<device_name>[^\/]+) \/ (?<port_num>[^]]+)\] \[dev_id: (?<device_guid>[a-f0-9]+)/.match(objtype)
+    match = /(?:Computer|Switch): (?<device_name>[^\/]+) \/ (?<port_num>[^\]]+)\] \[dev_id: (?<device_guid>[a-f0-9]+)/.match(objtype)
     if match
       # Extract and assign the captured groups to new fields
       record['device_name'] = match[:device_name]
       record['port_num'] = match[:port_num]
-      record['device_guid'] = match[:device_guid]
+      record['node_guid'] = match[:device_guid]
       # Assign the object_id to the matched part of the string
       record['object_id'] = "#{match[0].split('] [').first}"
 
@@ -21,7 +21,7 @@ def filter(tag, time, record)
       end
     end
   when 'Site'
-    record['object_id'] = 'default'
+    record['object_id'] = 'Site'
 #  when 'Computer'
 #    match = /(Computer.*)\] \[/.match(objtype)
 #    record['object_id'] = match ? match[1] : objtype
@@ -29,8 +29,19 @@ def filter(tag, time, record)
     match = /(Switch.*)\] \[/.match(objtype)
     record['object_id'] = match ? match[1] : objtype
   when 'Link'
-    match = /([a-f0-9]+\_[0-9]+)[\w ]+\: ([a-f0-9]+_[0-9]+)/.match(objtype)
-    record['object_id'] = match ? "#{match[1]}:#{match[2]}" : objtype
+    match = /(?<port_guid>[a-f0-9]+)_(?<port_num>[0-9]+)[\w ]+\: (?<link_partner_port_guid>[a-f0-9]+)_(?<link_partner_port_num>[0-9]+)/.match(objtype)
+    #    match = /([a-f0-9]+\_[0-9]+)[\w ]+\: ([a-f0-9]+_[0-9]+)/.match(objtype)
+    #    record['object_id'] = match ? "#{match[1]}:#{match[2]}" : objtype
+
+    if match
+      record['port_guid'] = match[:port_guid]
+      record['port_num'] = match[:port_num]
+      record['link_partner_port_guid'] = match[:link_partner_port_guid]
+      record['link_partner_port_num'] = match[:link_partner_port_num]
+      record['object_id'] = "#{match[:port_guid]}_#{match[:port_num]}:#{match[:link_partner_port_guid]}_#{match[:link_partner_port_num]}"
+    else
+      record['object_id'] = objtype
+    end
   else
     record['object_id'] = objtype
   end
@@ -42,7 +53,15 @@ def filter(tag, time, record)
 
   if event.nil? || event.empty?
     event = record['event_fallback']
-    event_details = record['event_fallback']
+  end
+
+  # Check and remove "None" from event and event_details
+  if event && event.include?("None")
+    event = event.gsub("None", "")
+  end
+
+  if event_details && event_details.include?("None")
+    event_details = event_details.gsub("None", "")
   end
 
   record['event'] = event
