52c87bbf8aadd32311c8e24aea54842950a5f5c2
Vitaly Gavensky <vgavensky@nvidia.com>
2024-10-22 18:11:34 +0300

    Enhancements

--- plugins/sysinfo_plugin/ufm_sim_web_service/ufm_web_service.py
+++ plugins/sysinfo_plugin/ufm_sim_web_service/ufm_web_service.py
@@ -65,11 +65,11 @@ def create_logger():
     if not os.path.exists(log_file):
         os.makedirs('/'.join(log_file.split('/')[:-1]), exist_ok=True)
 
+    # pylint: disable=line-too-long
     logger = logging.getLogger(log_file)
     format_str = f"%(asctime)-15s ufm-sysinfo-plugin-{log_file} Machine: localhost     %(levelname)-7s: %(message)s"
     logging.basicConfig(format=format_str, level=Configuration.log_level)
-    rotate_handler = RotatingFileHandler(log_file,maxBytes=Configuration.log_file_max_size,
-                                         backupCount=Configuration.log_file_backup_count)
+    rotate_handler = RotatingFileHandler(log_file,maxBytes=Configuration.log_file_max_size, backupCount=Configuration.log_file_backup_count)
     rotate_handler.setLevel(Configuration.log_level)
     rotate_handler.setFormatter(logging.Formatter(format_str))
     logger.addHandler(rotate_handler)
@@ -90,7 +90,7 @@ def main():
 
         server = BaseAiohttpServer(logger)
         server.run(api.app, host, port)
-    
+
     except Exception as e: # pylint: disable=broad-exception-caught
         logger.error(f"{str(e)}")
 
