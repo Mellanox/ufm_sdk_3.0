db5be8fb9d6b1e1e381a99fbb241e33d0fdcb618
Miryam Schwartz <miryam770s@gmail.com>
2024-11-14 14:35:23 +0200

    small fixes and copyright

--- plugins/fluentd_telemetry_plugin/src/merge_configuration_files.py
+++ plugins/fluentd_telemetry_plugin/src/merge_configuration_files.py
@@ -1,3 +1,18 @@
+"""
+@copyright:
+    Copyright (C) Mellanox Technologies Ltd. 2014-2020.  ALL RIGHTS RESERVED.
+
+    This software product is a proprietary product of Mellanox Technologies
+    Ltd. (the "Company") and all right, title, and interest in and to the
+    software product, including all associated intellectual property rights,
+    are and shall remain exclusively with the Company.
+
+    This software product is governed by the End User License Agreement
+    provided with the software product.
+
+@author: Miryam Schwartz
+@date:   Oct 14, 2024
+"""
 import configparser
 import logging
 import sys
@@ -6,10 +21,10 @@ import os
 def merge_ini_files(old_file_path, new_file_path):
     # Check if files exist
     if not os.path.isfile(old_file_path):
-        logging.error(f"file %s does not exist.", old_file_path)
+        logging.error("file %s does not exist.", old_file_path)
         sys.exit(1)
     if not os.path.isfile(new_file_path):
-        logging.error(f"file %s does not exist.", new_file_path)
+        logging.error("file %s does not exist.", new_file_path)
         sys.exit(1)
 
     # Create a configparser object
@@ -22,7 +37,7 @@ def merge_ini_files(old_file_path, new_file_path):
         config_new.read(new_file_path)
 
     except configparser.Error as e:
-        logging.error(f"Failed to parse configurations files: %s", e)
+        logging.error("Failed to parse configurations files: %s", e)
         sys.exit(1)
 
     # Merge configurations
@@ -39,7 +54,7 @@ def merge_ini_files(old_file_path, new_file_path):
     with open(old_file_path, 'w', encoding="utf-8") as configfile:
         config_old.write(configfile)
 
-    logging.info(f"Configuration has been merged and saved to %s", old_file_path)
+    logging.info("Configuration has been merged and saved to %s", old_file_path)
 
 if __name__ == "__main__":
     # Get file paths from command line arguments
