6cb90bb38e42d6f5324ebe46bcceba712a697e67
Miryam Schwartz <miryam770s@gmail.com>
2024-11-21 13:29:24 +0200

    change merge_conf_file path

--- plugins/fluentd_telemetry_plugin/scripts/upgrade.sh
+++ plugins/fluentd_telemetry_plugin/scripts/upgrade.sh
@@ -1,2 +1,2 @@
-#!bin/bash
-/bin/usr/python merge_configuration_files.py /config/fluentd_telemetry_conf.cfg /opt/ufm/ufm_plugin_tfs/fluentd_telemetry_plugin/conf/fluentd_telemetry_conf.cfg
+#!/bin/bash
+python3 /opt/ufm/ufm_plugin_tfs/ufm_sdk_tools/src/merge_configuration_files.py /config/fluentd_telemetry_conf.cfg /opt/ufm/ufm_plugin_tfs/fluentd_telemetry_plugin/conf/fluentd_telemetry_conf.cfg
