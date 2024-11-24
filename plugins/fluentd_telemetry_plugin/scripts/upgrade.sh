56282764655f078fc22473be6c583d165c029028
Miryam Schwartz <miryam770s@gmail.com>
2024-11-24 13:01:08 +0200

    delete merge_configuration_files.py

--- plugins/fluentd_telemetry_plugin/scripts/upgrade.sh
+++ plugins/fluentd_telemetry_plugin/scripts/upgrade.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 /opt/ufm/ufm_plugin_tfs/ufm_sdk_tools/src/config_parser_utils/merge_configuration_files.py /config/fluentd_telemetry_conf.cfg /opt/ufm/ufm_plugin_tfs/fluentd_telemetry_plugin/conf/fluentd_telemetry_conf.cfg
+python3 /opt/ufm/ufm_plugin_tfs/ufm_sdk_tools/src/config_parser_utils/merge_configuration_files.py /config/fluentd_telemetry_conf.cfg /opt/ufm/ufm_plugin_tfs/fluentd_telemetry_plugin/conf/fluentd_telemetry_plugin.cfg
