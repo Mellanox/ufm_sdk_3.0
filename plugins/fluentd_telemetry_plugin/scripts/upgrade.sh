1e4d7a7a73220872dcd0520f40b8a88dabadf529
Miryam-Schwartz <101129301+Miryam-Schwartz@users.noreply.github.com>
2025-02-03 10:22:06 +0200

    issue: 4273578: [TFS Plugin] Upgrading plugin with manage_ufm_plugins.sh upgrade command fails (#296)

--- plugins/fluentd_telemetry_plugin/scripts/upgrade.sh
+++ plugins/fluentd_telemetry_plugin/scripts/upgrade.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python3 /opt/ufm/ufm_plugin_tfs/ufm_sdk_tools/src/config_parser_utils/merge_configuration_files.py /config/fluentd_telemetry_conf.cfg /opt/ufm/ufm_plugin_tfs/fluentd_telemetry_plugin/conf/fluentd_telemetry_plugin.cfg
+python3 /opt/ufm/ufm_plugin_tfs/ufm_sdk_tools/src/config_parser_utils/merge_configuration_files.py /config/fluentd_telemetry_plugin.cfg /opt/ufm/ufm_plugin_tfs/fluentd_telemetry_plugin/conf/fluentd_telemetry_plugin.cfg
 exit $?
