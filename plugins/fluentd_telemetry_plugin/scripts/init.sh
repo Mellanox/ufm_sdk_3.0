c0d16572960da093a6f5caa38b844931b7a807a1
Elad Gershon <egershon@nvidia.com>
2025-03-17 11:52:17 +0200

    added readonly variables and fix comments
    Signed-off-by: Elad Gershon <egershon@nvidia.com>

--- plugins/fluentd_telemetry_plugin/scripts/init.sh
+++ plugins/fluentd_telemetry_plugin/scripts/init.sh
@@ -15,19 +15,33 @@
 
 set -eE
 
-CONFIG_FOLDER=/config
+# Define readonly variables for the plugin init script
+readonly PLUGIN_NAME="tfs"
+readonly TFS_PLUGIN_DIR="/opt/ufm/ufm_plugin_${PLUGIN_NAME}"
+readonly CONFIG_PATH="/config"
+readonly PLUGIN_DATA_PATH="/opt/ufm/ufm_plugins_data/${PLUGIN_NAME}"
+readonly SHARED_VOLUMES_PATH="${CONFIG_PATH}/${PLUGIN_NAME}_shared_volumes.conf"
 
-mkdir -p $CONFIG_FOLDER
-cp /opt/ufm/ufm_plugin_tfs/*.conf $CONFIG_FOLDER
-cp /opt/ufm/ufm_plugin_tfs/fluentd_telemetry_plugin.cfg $CONFIG_FOLDER
+# Create the config folder
+mkdir -p "$CONFIG_PATH"
 
+# Copy configuration files to the config folder
+cp "$TFS_PLUGIN_DIR"/*.conf $CONFIG_PATH
+cp "$TFS_PLUGIN_DIR"/fluentd_telemetry_plugin.cfg $CONFIG_PATH
+
+# If the file system is read-only non root
 if [[ "${READ_ONLY_FS}" == "true" ]]; then
-    PLUGIN_DATA_PATH=/opt/ufm/ufm_plugins_data/tfs
+    # Create temporary directories
+    for dir in /data/var/tmp /data/tmp; do
+        mkdir -p "${dir}"
+    done
+
+    # Generate the shared volumes configuration
     for vol in \
     "${PLUGIN_DATA_PATH}/tmp:/tmp" 
     do
-        echo "$vol" >> "$CONFIG_FOLDER/tfs_shared_volumes.conf"
+        echo "$vol" >> "$SHARED_VOLUMES_PATH"
     done
 fi 
 
-exit 1
+exit 0
