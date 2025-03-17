dc86257c83f2f8703859f7e8d6ed2241045bd47c
Maor Walter <mwalter@nvidia.com>
2025-03-17 11:35:01 +0200

    Adding missing script block to DOCKERHUB_UPLOADER
    Signed-off-by: Maor Walter <mwalter@nvidia.com>

--- .ci/jenkinsfile_hub_uploader.groovy
+++ .ci/jenkinsfile_hub_uploader.groovy
@@ -15,32 +15,34 @@ pipeline{
         }
         stage('Upload to docker-hub'){
             steps{
-                withCredentials([usernamePassword(credentialsId: '0fbf63c0-4a61-4543-811d-a182df47711b', usernameVariable: 'DH_USER', passwordVariable: 'DH_TOKEN' )]){
-                    wrap([$class: 'BuildUser']) {
-                        def authorized_users = ["bitkin", "afok", "kobib", "drorl", "tlerner", "omarj", "samerd", "atolikin", "atabachnik", "eylonk", "lennyv", "asafb", "sspormas", "mkianovsky", "asafb", "samerd"]
-                        def BUILD_USER_ID = env.BUILD_USER_ID.tokenize("@")[0]
-                        if (!authorized_users.contains(BUILD_USER_ID)) {
-                            error """${BUILD_USER_ID} is not authorized to upload images to Docker Hub.
-Please contact one of the approved users to upload a container: ${authorized_users.join(', ')}"""
+                script {
+                    withCredentials([usernamePassword(credentialsId: '0fbf63c0-4a61-4543-811d-a182df47711b', usernameVariable: 'DH_USER', passwordVariable: 'DH_TOKEN' )]){
+                        wrap([$class: 'BuildUser']) {
+                            def authorized_users = ["bitkin", "afok", "kobib", "drorl", "tlerner", "omarj", "samerd", "atolikin", "atabachnik", "eylonk", "lennyv", "asafb", "sspormas", "mkianovsky", "asafb", "samerd"]
+                            def BUILD_USER_ID = env.BUILD_USER_ID.tokenize("@")[0]
+                            if (!authorized_users.contains(BUILD_USER_ID)) {
+                                error """${BUILD_USER_ID} is not authorized to upload images to Docker Hub.
+    Please contact one of the approved users to upload a container: ${authorized_users.join(', ')}"""
+                            }
                         }
+                        sh '''#!/bin/bash -xveE
+                        printenv
+                        cmd_args=''
+                        if [ -n "${IMAGE_NAME}" ]; then
+                            cmd_args="${cmd_args}-i ${IMAGE_NAME} "
+                        fi
+                        if [ -n "${IMAGE_TAG}" ]; then
+                            cmd_args="${cmd_args}-t ${IMAGE_TAG} "
+                        fi
+                        if [ "${FORCE_PUSH}" == "true" ]; then
+                            cmd_args="${cmd_args}-f "
+                        fi
+                        if [ "${LATEST}" == "true" ]; then
+                            cmd_args="${cmd_args}-l "
+                        fi
+                        python3 .ci/dockerhub_uploader.py -u ${DH_USER} -p ${DH_TOKEN} -d ${IMAGE_PATH} ${cmd_args}
+                        '''
                     }
-                    sh '''#!/bin/bash -xveE
-                    printenv
-                    cmd_args=''
-                    if [ -n "${IMAGE_NAME}" ]; then
-                        cmd_args="${cmd_args}-i ${IMAGE_NAME} "
-                    fi
-                    if [ -n "${IMAGE_TAG}" ]; then
-                        cmd_args="${cmd_args}-t ${IMAGE_TAG} "
-                    fi
-                    if [ "${FORCE_PUSH}" == "true" ]; then
-                        cmd_args="${cmd_args}-f "
-                    fi
-                    if [ "${LATEST}" == "true" ]; then
-                        cmd_args="${cmd_args}-l "
-                    fi
-                    python3 .ci/dockerhub_uploader.py -u ${DH_USER} -p ${DH_TOKEN} -d ${IMAGE_PATH} ${cmd_args}
-                    '''
                 }
             }
         }
