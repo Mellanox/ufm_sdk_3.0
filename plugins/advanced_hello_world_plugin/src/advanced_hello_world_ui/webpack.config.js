b8a66036ff2704a123090c7c92f0813e43db0459
ananalaghbar <anana@nvidia.com>
2026-01-13 16:00:30 +0200

    Fixing the advanced_hello_world build

--- plugins/advanced_hello_world_plugin/src/advanced_hello_world_ui/webpack.config.js
+++ plugins/advanced_hello_world_plugin/src/advanced_hello_world_ui/webpack.config.js
@@ -1,4 +1,4 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+const { share, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
 
@@ -11,8 +11,11 @@ module.exports = withModuleFederationPlugin({
     'DeviceContextMenuModule': './src/app/hello-world/devices-context-menu/devices-context-menu.module.ts'
   },
 
-  shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
-  },
+  shared: share({
+    "@angular/core": {singleton: true, strictVersion: true, requiredVersion: 'auto'},
+    "@angular/common": {singleton: true, strictVersion: true, requiredVersion: 'auto'},
+    "@angular/common/http": {singleton: true, strictVersion: true, requiredVersion: 'auto'},
+    "@angular/router": {singleton: true, strictVersion: true, requiredVersion: 'auto'}
+  }),
 
 });
